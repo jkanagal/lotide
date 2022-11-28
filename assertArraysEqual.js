5c97c060dd7680c18dab021b6f1071145b7d4eb9
Jana K <31860875+jkanagal@users.noreply.github.com>
2022-11-27 22:17:12 -0500

    assertarrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,30 @@
+
+// const eqArrays = function (arr1, arr2) {
+//   let ans;
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr1[i] === arr2[i]) {
+//       ans = true;
+//     } else {
+//       return ans = false;
+//     }
+//   } return ans;
+// };
+
+const eqArrays = function (arr1, arr2) {
+  let ans = true;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      ans = false;
+    }
+  } return ans;
+};
+
+const assertArraysEqual = function(array1,array2) {
+  const check = String.fromCodePoint(0x2705);
+  const wrong = String.fromCodePoint(0x274C);
+  return (eqArrays(array1,array2)) ? `Assertion Pass: ${check} ` : `Assertion failed: ${wrong}`;
+};
+
+//why does having console.log in the return give us undefined?
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3])); // => true
+console.log(assertArraysEqual([1, 2, 3], [1, 3, 3]));
