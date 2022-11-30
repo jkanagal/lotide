4311fbeebee668a619a9cb1ca5380ef7238c7932
Jana K <31860875+jkanagal@users.noreply.github.com>
2022-11-30 01:13:10 -0500

    middle fn

--- middle.js
+++ middle.js
@@ -13,14 +13,30 @@ const assertArraysEqual = function (array1, array2) {
   return (eqArrays(array1, array2)) ? `Assertion Pass: ${check} ` : `Assertion failed: ${wrong}`;
 };
 
-const middle = function(arr1){
-
+const middle = function (arr1) {
+  let newArr = [];
+  let newArr1;
+  let mid;
+  if (arr1.length === 1 || arr1.length === 2) {
+    return newArr;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1.length % 2 === 1) {
+        mid = [arr1[Math.floor((arr1.length / 2))]];
+      } else {
+        newArr = [arr1[(arr1.length / 2) - 1]];
+        newArr1 = [arr1[(arr1.length / 2)]];
+        mid = newArr.concat(newArr1);
+      }
+      return mid;
+    }
+  }
 };
 
 
-middle([1]); // => []
-middle([1, 2]);
-middle([1, 2, 3]); // => [2]
-middle([1, 2, 3, 4, 5]); // => [3]
-middle([1, 2, 3, 4]) // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
+console.log(middle([1])); // => []
+console.log(middle([1, 2]));
+console.log(middle([1, 2, 3])); // => [2]
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
