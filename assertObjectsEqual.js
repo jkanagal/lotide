07142d96b4ea1e69317e82e22edf34e72bb4d9c1
Jana K <31860875+jkanagal@users.noreply.github.com>
2022-12-18 21:17:03 -0500

    changes

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -17,11 +17,15 @@ const assertArraysEqual = function (array1, array2) {
 
 const eqObjects = function (object1, object2) {
   if (Object.keys(object1).length === Object.keys(object2).length) {
-    for (const key in object1) {
-      if (object1[key] === object2[key]) {
-        return true;
-      } else {
-        return false;
+    if (Array.isArray(object1) && Array.isArray(object2)) {
+      eqArrays(object1,object2);
+    } else {
+      for (const key in object1) {
+        if (object1[key] === object2[key]) {
+          return true;
+        } else {
+          return false;
+        }
       }
     }
   } else {
@@ -30,10 +34,10 @@ const eqObjects = function (object1, object2) {
 };
 
 const assertObjectsEqual = function (actual, expected) {
-  // const inspect = require('util').inspect;
+  const inspect = require('util').inspect;
   const check = String.fromCodePoint(0x2705);
   const wrong = String.fromCodePoint(0x274C);
-  return (eqObjects(actual, expected)) ? `Assertion Pass: ${check} ` : `Assertion failed: ${wrong}`;
+  return (eqObjects(actual, expected) ? `Assertion Passed ${check}: ${inspect(actual)} === ${inspect(expected)}` : `Assertion Failed ${wrong}: ${inspect(actual)} === ${inspect(expected)}`);
 };
 
 const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
